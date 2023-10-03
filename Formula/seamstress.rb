a48071be3424a8d3338ceaebe25c0115261cc47a
Rylee Lyman <rylee.alanza@gmail.com>
2023-10-02 22:25:36 -0700

    feat: add seamstress tap

---
+++ Formula/seamstress.rb
@@ -0,0 +1,21 @@
+class Seamstress < Formula
+  desc "Lua scripting environment for musical communication"
+  homepage "https://github.com/ryleelyman/seamstress"
+  url "https://github.com/ryleelyman/seamstress/archive/refs/tags/v0.24.0.tar.gz"
+  sha256 "813d0fdb117af0cb6b9c3222f921eb4f397bc329400b63ec17f6f644978deb0c"
+  license "GPL v3"
+
+  depends_on "zig" => :build
+  depends_on "freetype"
+  depends_on "ncurses"
+  depends_on "harfbuzz"
+
+  def install
+    system "zig", "build", "install", "-Doptimize=ReleaseFast", "--prefix", "#{prefix}"
+  end
+
+  test do
+    system "echo", "_seamstress.quit_lvm\\(\\)", ">>", "#{bin}/_seamstress_test.lua"
+    system "#{bin}/seamstress", "#{bin}/_seamstress_test"
+  end
+end
