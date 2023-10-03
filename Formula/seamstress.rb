5145c7535b967461582c3a7e94d9d96e5cb5e6fc
Rylee Lyman <rylee.alanza@gmail.com>
2023-10-02 22:41:30 -0700

    fix: audit

--- Formula/seamstress.rb
+++ Formula/seamstress.rb
@@ -3,15 +3,16 @@ class Seamstress < Formula
   homepage "https://github.com/ryleelyman/seamstress"
   url "https://github.com/ryleelyman/seamstress/archive/refs/tags/v0.24.0.tar.gz"
   sha256 "813d0fdb117af0cb6b9c3222f921eb4f397bc329400b63ec17f6f644978deb0c"
-  license "GPL v3"
+  license "GPL-3.0-or-later"
 
   depends_on "zig" => :build
   depends_on "freetype"
-  depends_on "ncurses"
   depends_on "harfbuzz"
+  depends_on "ncurses"
+
 
   def install
-    system "zig", "build", "install", "-Doptimize=ReleaseFast", "--prefix", "#{prefix}"
+    system "zig", "build", "install", "-Doptimize=ReleaseFast", "--prefix", prefix.to_s
   end
 
   test do
