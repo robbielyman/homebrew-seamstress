16757316f4dca565cebb2e7f5b7a62bd0a53ae34
Rylee Lyman <rylee.alanza@gmail.com>
2023-10-03 23:13:03 -0700

    fix: attempt to correctly build

--- Formula/seamstress.rb
+++ Formula/seamstress.rb
@@ -1,8 +1,8 @@
 class Seamstress < Formula
   desc "Lua scripting environment for musical communication"
   homepage "https://github.com/ryleelyman/seamstress"
-  url "https://github.com/ryleelyman/seamstress/archive/refs/tags/v0.24.1.tar.gz"
-  sha256 "5f5d435c2b9d4278d11bd45b34b7468d81ec2f6ac275a92d1210e0b24f77f0b4"
+  url "https://github.com/ryleelyman/seamstress/archive/refs/tags/v0.24.2.tar.gz"
+  sha256 "8042042b66e11f304da887a2a2b036f20e5765cbb61b3ca933623808afc5283b"
   license "GPL-3.0-or-later"
 
   depends_on "pkg-config" => :build
@@ -12,12 +12,27 @@ class Seamstress < Formula
   depends_on "libpng"
   depends_on "ncurses"
 
+  on_linux do
+    depends_on "alsa-lib"
+    depends_on "dbus"
+    depends_on "jack"
+    depends_on "libdrm"
+    depends_on "libsamplerate"
+    depends_on "libxcb"
+    depends_on "libxcursor"
+    depends_on "libxext"
+    depends_on "libxi"
+    depends_on "libxkbcommon"
+    depends_on "libxrandr"
+    depends_on "wayland"
+  end
+
   def install
     system "zig", "build", "install", "--verbose", "-Doptimize=ReleaseFast", "--prefix", prefix.to_s
   end
 
   test do
-    system "echo", "_seamstress.quit_lvm\\(\\)", ">>", "#{bin}/_seamstress_test.lua"
-    system "#{bin}/seamstress", "#{bin}/_seamstress_test"
+    system "echo", "_seamstress.quit_lvm\\(\\)", ">>", "#/tmp/_seamstress_test.lua"
+    system "#{bin}/seamstress", "#/tmpn/_seamstress_test"
   end
 end
