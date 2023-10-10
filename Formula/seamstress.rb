ef7cc6e4f48826ba5e00050022332dd02b27d335
Rylee Alanza Lyman <46907231+ryleelyman@users.noreply.github.com>
2023-10-10 14:58:00 -0400

    fix: seamstress test

--- Formula/seamstress.rb
+++ Formula/seamstress.rb
@@ -46,7 +46,7 @@ class Seamstress < Formula
   test do
     test_str = <<~EOF
       SEAMSTRESS
-      seamstress version: 0.25.1
+      seamstress version: 0.25.2
       > > seamstress was unable to find user-provided script.lua file!
       > create such a file and place it in either CWD or ~/seamstress
       > SEAMSTRESS: goodbye
