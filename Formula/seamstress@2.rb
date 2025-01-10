16857393b7abb2ee0df262de9344d867f13b4e1c
Robbie Lyman <rb.lymn@gmail.com>
2025-01-09 20:01:27 -0500

    fix: style

--- Formula/seamstress@2.rb
+++ Formula/seamstress@2.rb
@@ -8,17 +8,17 @@ class SeamstressAT2 < Formula
   depends_on "zig" => :build
   depends_on "busted" => :test
 
-
   def install
     system "zig", "build", "install", "--verbose", "-Doptimize=ReleaseSafe", "--prefix", prefix.to_s
   end
 
   test do
-    system "echo", "#{Formula["busted"].libexec}/share/lua/5.4/?/?.lua;#{Formula["busted"].libexec}/share/lua/5.4/?.lua;#{Formula["busted"].libexec}/share/lua/5.4/?/init.lua"
-    system "echo", "#{Formula["busted"].libexec}/lib/lua/5.4/?.so"
+    lua_path_1 = "#{Formula["busted"].libexec}/share/lua/5.4/?/?.lua"
+    lua_path_2 = "#{Formula["busted"].libexec}/share/lua/5.4/?.lua"
+    lua_path_3 = "#{Formula["busted"].libexec}/share/lua/5.4/?/init.lua"
     with_env(
-      "LUA_PATH" => "#{Formula["busted"].libexec}/share/lua/5.4/?/?.lua;#{Formula["busted"].libexec}/share/lua/5.4/?.lua;#{Formula["busted"].libexec}/share/lua/5.4/?/init.lua",
-      "LUA_CPATH" => "#{Formula["busted"].libexec}/lib/lua/5.4/?.so"
+      "LUA_PATH"  => "#{lua_path_1};#{lua_path_2};#{lua_path_3}",
+      "LUA_CPATH" => "#{Formula["busted"].libexec}/lib/lua/5.4/?.so",
     ) do
       assert_includes shell_output("#{bin}/seamstress --test"), "0 failures / 0 errors"
     end
